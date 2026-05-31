9ae67ec20d122ae7f5007ef0d04b3edf1df4e87d
minhtran291 <minhtran2912003@gmail.com>
2026-05-31 14:48:38 +0700

    add validator

---
+++ SSMS.Application/Validator/Product/CreateProductDTOValidator.cs
@@ -0,0 +1,93 @@
+﻿using FluentValidation;
+using SSMS.Application.DTOs.Product;
+
+namespace SSMS.Application.Validator.Product
+{
+    public class CreateProductDTOValidator : AbstractValidator<CreateProductDTO>
+    {
+        public CreateProductDTOValidator()
+        {
+            RuleFor(x => x.ProductName)
+                .NotEmpty()
+                .WithMessage("Tên sản phẩm không được để trống")
+                .MaximumLength(200)
+                .WithMessage("Tên sản phẩm không được vượt quá 200 ký tự");
+
+            RuleFor(x => x.CategoryId)
+                .GreaterThan(0)
+                .WithMessage("Vui lòng chọn một danh mục hợp lệ");
+
+            RuleFor(x => x.BrandId)
+                .GreaterThan(0)
+                .WithMessage("Vui lòng chọn một thương hiệu hợp lệ");
+
+            RuleFor(x => x.SizePrices)
+                .NotEmpty()
+                .WithMessage("Vui lòng thêm ít nhất một kích thước và giá")
+                .Must(x => x.Select(s => s.SizeId).Distinct().Count() == x.Count)
+                .WithMessage("Kích thước không được trùng lặp");
+
+            RuleFor(x => x.Images)
+                .NotEmpty()
+                .WithMessage("Vui lòng thêm ít nhất một hình ảnh sản phẩm")
+                .Must(x => x.Select(i => i.DisplayOrder).Distinct().Count() == x.Count)
+                .WithMessage("Thứ tự hiển thị ảnh không được trùng lặp");
+
+            RuleForEach(x => x.SizePrices)
+                .SetValidator(new CreateProductSizePriceDTOValidator());
+
+            RuleForEach(x => x.Images)
+                .SetValidator(new CreateProductImageDTOValidator());
+        }
+    }
+
+    public class CreateProductSizePriceDTOValidator : AbstractValidator<CreateProductSizePriceDTO>
+    {
+        public CreateProductSizePriceDTOValidator()
+        {
+            RuleFor(x => x.SizeId)
+                .GreaterThan(0)
+                .WithMessage("Vui lòng chọn một kích thước hợp lệ");
+
+            RuleFor(x => x.Price)
+                .GreaterThan(0)
+                .WithMessage("Giá phải lớn hơn 0");
+        }
+    }
+
+    public class CreateProductImageDTOValidator : AbstractValidator<CreateProductImageDTO>
+    {
+        private static readonly string[] AllowedExtensions =
+        {
+            ".jpg", 
+            ".jpeg", 
+            ".png", 
+            ".webp"
+        };
+
+        public CreateProductImageDTOValidator()
+        {
+            RuleFor(x => x.Image)
+                .Cascade(CascadeMode.Stop)
+                .NotNull()
+                .WithMessage("Hình ảnh không được để trống")
+
+                .Must(file => file is not null && file.Length > 0)
+                .WithMessage("Hình ảnh không được để trống")
+                .Must(file =>
+                {
+                    return AllowedExtensions.Contains(
+                        Path.GetExtension(file.FileName), 
+                        StringComparer.OrdinalIgnoreCase);
+                })
+                .WithMessage("Định dạng ảnh không hợp lệ")
+
+                .Must(file => file.Length <= 5 * 1024 * 1024)
+                .WithMessage("Kích thước ảnh tối đa là 5MB");
+
+            RuleFor(x => x.DisplayOrder)
+                .GreaterThanOrEqualTo(0)
+                .WithMessage("Thứ tự hiển thị phải lớn hơn hoặc bằng 0");
+        }
+    }
+}
