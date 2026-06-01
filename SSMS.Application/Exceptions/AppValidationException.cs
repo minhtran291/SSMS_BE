b64825cf4e437af38fb801fca8eb7ae873b5aac4
minhtran291 <minhtran2912003@gmail.com>
2026-06-01 15:48:46 +0700

    update exception middleware and add custom validation exception

---
+++ SSMS.Application/Exceptions/AppValidationException.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace SSMS.Application.Exceptions
+{
+    public class AppValidationException : BaseException
+    {
+        public Dictionary<string, string[]> Errors { get; }
+
+        public AppValidationException(Dictionary<string, string[]> errors) 
+            : base("Validation Failed", StatusCodes.Status400BadRequest)
+        {
+            Errors = errors;
+        }
+    }
+}
