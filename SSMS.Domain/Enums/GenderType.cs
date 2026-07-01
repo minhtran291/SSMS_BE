785a55b63f786c779f128fa83eb2ba1d8843f91d
minhtran291 <minhtran2912003@gmail.com>
2026-07-01 13:18:06 +0700

    modify seed data

--- SSMS.Domain/Enums/GenderType.cs
+++ SSMS.Domain/Enums/GenderType.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SSMS.Domain.Enums
+﻿namespace SSMS.Domain.Enums
 {
     public enum GenderType : byte
     {
