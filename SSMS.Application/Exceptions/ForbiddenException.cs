ad811206f115f82be6fa5ccf04eb04ebf617655a
minhtran291 <minhtran2912003@gmail.com>
2026-07-14 15:16:35 +0700

    add logout feature

---
+++ SSMS.Application/Exceptions/ForbiddenException.cs
@@ -0,0 +1,9 @@
+﻿using System.Net;
+
+namespace SSMS.Application.Exceptions
+{
+    public class ForbiddenException : BaseException
+    {
+        public ForbiddenException(string message) : base(message, (int)HttpStatusCode.Forbidden) { }
+    }
+}
---
+++ Shared.Contracts/Authentication/CurrentUser.cs
@@ -0,0 +1,10 @@
+﻿namespace Shared.Contracts.Authentication
+{
+    public class CurrentUser
+    {
+        public string KeycloakUserId { get; init; } = string.Empty;
+        public string UserName { get; init; } = string.Empty;
+        public string FullName { get; init; } = string.Empty;
+        //public IReadOnlyCollection<string> Roles { get; init; } = [];
+    }
+}
